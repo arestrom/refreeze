6153ad0d72652dabbda7df4d160a7e3799efd726
Are Strom <are.strom@gmail.com>
2021-02-12 12:34:20 -0800

    Updated dependencies script for simplicity

--- find_package_dependencies.R
+++ find_package_dependencies.R
@@ -1,21 +1,22 @@
 #=========================================================================
 # Identify all package dependencies...includes recursive search
 #
-# AS 2020-09-28
+# AS 2021-02-12
 #=========================================================================
 
 # # Add list of libraries from global
 # library(shiny)
 # library(shinydashboard)
 # library(shinydashboardPlus)
-# library(shinyTime)
+# library(shinyWidgets)
 # library(shinyjs)
+# library(shinyTime)
 # library(tippy)
-# library(RPostgres)
 # library(glue)
+# library(pool)
 # library(tibble)
 # library(DBI)
-# library(pool)
+# library(RPostgres)
 # library(dplyr)
 # library(DT)
 # library(leaflet)
@@ -29,27 +30,34 @@
 # library(stringi)
 
 # Get list of dependencies
-lib_pkg = c("shiny", "shinydashboard", "shinydashboardPlus",
-            "shinyTime", "shinyjs", "tippy", "RPostgres",
-            "glue", "tibble", "DBI", "pool", "dplyr", "DT",
-            "leaflet", "mapedit", "leaflet.extras", "sf",
-            "lubridate", "uuid", "shinytoastr", "shinycssloaders",
-            "stringi")
+lib_pkg = c("shiny", "shinydashboard", "shinydashboardPlus", "shinyWidgets",
+            "shinyjs", "shinyTime", "tippy", "glue", "tibble", "DBI", "RPostgres",
+            "dplyr", "DT", "leaflet", "mapedit", "leaflet.extras", "sf", "lubridate",
+            "uuid", "shinytoastr", "shinycssloaders", "stringi")
 dep_pkg = tools::package_dependencies(lib_pkg, recursive = TRUE)
 dep_vec = unique(as.vector(unlist(dep_pkg)))
 all_pkg = unique(c(unique(lib_pkg), dep_vec))
-all_pkg
+sort(all_pkg)
 
 # Find all packages in base
 base_pkg = rownames(installed.packages(priority = "base"))
-base_pkg
+sort(base_pkg)
 
-# Pull out only packages not in base
-needed_pkg = all_pkg[!all_pkg %in% base_pkg]
+# Find all newly installed packages in package library
+rp_lib = .libPaths()[2]
+rp_lib
+new_pkg = rownames(installed.packages(rp_lib, priority = "NA"))
+sort(new_pkg)
+
+# Pull out only packages not in base or new_pkg
+needed_pkg = all_pkg[!all_pkg %in% c(new_pkg, base_pkg)]
 sort(needed_pkg)
+
+# Print full set needed
+paste0(paste0("'", needed_pkg, "'", collapse = ", "))
 
 # Find full set of installed packages...after installing any missing ones
 final_pkg = rownames(installed.packages())
 
 # Check if all_pkg are now in final_pkg....need to test this last line
-all(final_pkg %in% final_pkg)
+all(all_pkg %in% final_pkg)
